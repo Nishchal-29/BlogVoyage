eb699872b2224d18f589ec77f23828d2a455e4cb
Nishchal Singh <nishchalsingh9a@gmail.com>
2025-06-17 16:47:27 +0530

    add forms and validators

--- blog.py
+++ blog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, flash
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '58d3c937cd5a32b6e287826b8e706791d06e5c61'
 
 posts = [
     {
@@ -30,6 +33,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'adminblog@example.com' and form.password.data == 'password':
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed. Please check your email and password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
